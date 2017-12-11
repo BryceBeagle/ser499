59bd40ea4c533677ead377227337a8246fc20591
ignormies <bryce.beagle@gmail.com>
2017-12-11 01:57:36 -0700

    Little more info for input3

--- test_files/input3.py
+++ test_files/input3.py
@@ -1,9 +1,20 @@
+# Functions with parameters demonstration
+# Also uses the return value of the function
+
+
 def test(a, b, c):
 
     x = a + b + c
     return x
 
 
-d = test(1, 2, 3)
+d = test(1, 2, 4)
 
 # Output = 6 @ mem[1]
+
+
+# Not working:
+#    Using multiple functions in one expression
+#        d = test(1, 2, 3) + test(5, 6, 7)
+#    Nesting functions
+#        d = test(test(5, 6, 7), 2, 3)
